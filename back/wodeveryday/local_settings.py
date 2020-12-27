586a5a93d39bce2199a730a92f8bba926019742e
ezl <ericzliu@gmail.com>
2020-12-27 17:14:28 -0600

    allow cross site locally

---
+++ back/wodeveryday/local_settings.py
@@ -0,0 +1,11 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
+
