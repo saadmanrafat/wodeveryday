a7820da720a2bac7809052177b869d26fc157cae
ezl <ericzliu@gmail.com>
2020-12-27 13:09:01 -0600

    first pass at gym models

--- back/wodeveryday/gyms/models.py
+++ back/wodeveryday/gyms/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django_extensions.db.fields import AutoSlugField
 
-# Create your models here.
+
+class Region(models.Model):
+    name = models.CharField(max_length=255)
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    region = models.ForeignKeyField(Region)
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKeyField(Country)
+
+class Gym(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name')
+    city = models.ForeignKeyField(City)
+
+    website = models.URLField(blank=True)
+    photo = models.URLField(blank=True)
+
+    status = models.BooleanField()
+    photo_version = models.BooleanField()
+    ready_to_link = models.BooleanField()
+    ordernum = models.IntegerField()
+    bad_standing = models.BooleanField()
+    zip = models.CharField(blank=True, max_length=15)
+    country_short_code = models.CharField(max_length=15)
+    address = models.TextField(blank=True)
+    lat = models.TextField(blank=True)
+    lon = models.TextField(blank=True)
+    aid = models.IntegerField()
+    org_type = models.CharField(blank=True, max_length=255)
+    effective_date = models.DateTimeField()
+    active = models.BooleanField()
+    show_on_map = models.BooleanField()
+    kids = models.BooleanField()
+    name_search = models.CharField(blank=True, max_length=255)
+    state_code = models.CharField(blank=True, max_length=15)
+    full_state = models.CharField(blank=True, max_length=255)
+
+
