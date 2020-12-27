b25efa1c1f96b98b12390ddb26737b00f7191e95
ezl <ericzliu@gmail.com>
2020-12-27 16:01:09 -0600

    add city/country/region models to admin

--- back/wodeveryday/gyms/admin.py
+++ back/wodeveryday/gyms/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Gym
+from .models import (
+    Region,
+    Country,
+    City,
+    Gym,
+)
 
+admin.site.register(Region)
+admin.site.register(Country)
+admin.site.register(City)
 admin.site.register(Gym)
 
 # Register your models here.
