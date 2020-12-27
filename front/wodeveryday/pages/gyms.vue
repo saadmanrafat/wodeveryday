3704dbc13ef3ed688e994ad99e3d35af31b14135
ezl <ericzliu@gmail.com>
2020-12-27 17:08:46 -0600

    Use axios to retrieve data

--- front/wodeveryday/pages/gyms.vue
+++ front/wodeveryday/pages/gyms.vue
@@ -4,11 +4,29 @@
       List of gyms
     </h1>
     <p>gyms gyms gyms</p>
+    <section>
+      <h1>Data fetched using asyncData</h1>
+      <ul>
+        <li v-for="mountain in mountains" :key="mountain.title">
+          <NuxtLink
+            :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }"
+          >
+            {{ mountain.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
+
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    const mountains = await $axios.$get('https://api.nuxtjs.dev/mountains')
+    return { mountains }
+  }
+}
 </script>
 
 <style>
